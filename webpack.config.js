d314de3682c584627fae5b6843841ef1398905ea
Caven Chen <cavencj@gmail.com>
2020-09-12 15:12:43 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,128 @@
+/**
+ * @Author: Caven
+ * @Date: 2020-01-18 19:22:23
+ */
+
+const path = require('path')
+const webpack = require('webpack')
+const packageInfo = require('./package.json')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const JavaScriptObfuscator = require('webpack-obfuscator')
+
+
+function resolve(dir) {
+    return path.join(__dirname, '.', dir)
+}
+
+function getTime() {
+    let now = new Date()
+    let m = now.getMonth() + 1
+    m = m < 10 ? '0' + m : m
+    let d = now.getDate()
+    d = d < 10 ? '0' + d : d
+    let h = now.getHours()
+    h = h < 10 ? '0' + h : h
+    let min = now.getMinutes()
+    min = min < 10 ? '0' + min : min
+    let s = now.getSeconds()
+    s = s < 10 ? '0' + s : s
+    return `${now.getFullYear()}-${m}-${d} ${h}:${min}:${s}`
+}
+
+module.exports = env => {
+    const IS_PROD = (env && env.production) || false
+    const publicPath = IS_PROD ? '/' : '/'
+    let plugins = [
+
+        new MiniCssExtractPlugin({
+            filename: IS_PROD ? '[name].min.css' : '[name].css',
+            allChunks: true
+        }),
+        new webpack.DefinePlugin({
+            'build.version': JSON.stringify(packageInfo.version),
+            'build.time': JSON.stringify(getTime())
+        })
+    ]
+    if (IS_PROD) {
+        plugins.push(new OptimizeCssAssetsPlugin())
+        plugins.push(new webpack.NoEmitOnErrorsPlugin())
+        plugins.push(
+            new JavaScriptObfuscator(
+                {
+                    rotateStringArray: true
+                },
+                []
+            )
+        )
+    }
+    return {
+        entry: {
+            'dc.base': ['base'],
+            'dc.core': ['entry', 'theme']
+        },
+        devtool: IS_PROD ? false : 'cheap-module-eval-source-map',
+        output: {
+            filename: IS_PROD ? '[name].min.js' : '[name].js',
+            path: path.resolve(__dirname, 'dist'),
+            publicPath: publicPath,
+            sourcePrefix: ''
+        },
+        module: {
+            unknownContextCritical: false,
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: ['@babel/transform-runtime'],
+                        compact: false,
+                        ignore: ['checkTree']
+                    }
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        {
+                            loader: 'css-loader'
+                        },
+                        {
+                            loader: 'sass-loader'
+                        }
+                    ]
+                },
+                {
+                    test: /\.scss$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        {
+                            loader: 'css-loader'
+                        },
+                        {
+                            loader: 'sass-loader'
+                        }
+                    ]
+                },
+                {
+                    test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+                    loader: 'url-loader',
+                    options: {
+                        limit: 20000
+                    }
+                }
+
+            ]
+        },
+        resolve: {
+            extensions: ['.js', '.json', '.css'],
+            alias: {
+                '@': resolve('src'),
+                entry: './src/index.js'
+            }
+        },
+        plugins
+    }
+}
